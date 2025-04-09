3ba7fc228de3d70360ca5f115f976bbc7221e144
junbox98221 <junbox98221@gmail.com>
2025-04-09 10:22:28 +0900

    프로그래머스: 가장 큰 수

---
+++ 프로그래머스/가장 큰 수.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+  const answer = numbers
+    .map(String)
+    .sort((a, b) => (b + a) * 1 - (a + b) * 1)
+    .join("");
+
+  return answer[0] === "0" ? "0" : answer;
+}
